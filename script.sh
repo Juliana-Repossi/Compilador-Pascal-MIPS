c092917c1686142ea3f1808f2719690fc76618bc
gomesfilipe <filipejvmf@gmail.com>
2023-06-17 15:02:22 -0300

    script para gerar pdf da ast

--- script.sh
+++ script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# ROOT=/home/filipe/git/compiladores-labs/lab5
+ROOT=/home/filipe/git/Compilador-Pascal-MIPS
 # ROOT=/home/fgasouza/compiladores-labs/lab5
-ROOT=/home/elaine/git/Compilador-Pascal-MIPS
+# ROOT=/home/elaine/git/Compilador-Pascal-MIPS
 ANTLR_PATH=$ROOT/tools/antlr-4.11.1-complete.jar
 CLASS_PATH_OPTION="-cp .:$ANTLR_PATH"
 
 BIN_PATH=bin
 
-DATA=$ROOT/
-IN=$DATA/testes/assignments
+DATA=$ROOT
+IN=$DATA/testes/cp5/statement/read_write
 # OUT=$DATA/out05
 MYOUT=$DATA/myout
 
@@ -18,5 +18,6 @@ for infile in ls $IN/*.pas; do
     dotfile=$MYOUT/${base/.pas/.dot}
     # dotfileout=$OUT/${base/.ezl/.dot}
     make run < $infile 2> $dotfile > /dev/null
+    dot -Tpdf $dotfile -o $MYOUT/$base.pdf 
     # diff -w $dotfile $dotfileout
 done
