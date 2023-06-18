0f175789633284372fd37cd86783497adccb8809
gomesfilipe <filipejvmf@gmail.com>
2023-06-18 16:23:31 -0300

    Adaptando script para executar todos os testes de uma vez.

--- script.sh
+++ script.sh
@@ -9,13 +9,32 @@ BIN_PATH=bin
 TEST_PATH=function
 
 DATA=$ROOT
-IN=$DATA/in/cp5/$TEST_PATH
+# IN=$DATA/in/cp5/$TEST_PATH
 OUT=$DATA/out
+IN=$DATA/in/cp5
 
-for infile in ls $IN/*.pas; do
-    base=$(basename $infile)
-    echo Running $base
-    dotfile=$OUT/${base/.pas/.dot}
-    make run < $infile 2> $dotfile > /dev/null
-    dot -Tpdf $dotfile -o $OUT/$base.pdf 
-done
+for dir in $IN/*; do
+    basedir=$(basename $dir)
+    
+    if [ ! -d $OUT/$basedir ];
+    then
+        mkdir $OUT/$basedir
+    fi
+
+    for infile in $dir/*.pas; do
+        base=$(basename $infile)
+        echo Running $base
+        dotfile=$OUT/$basedir/${base/.pas/.dot}
+        pdffile=$OUT/$basedir/${base/.pas/.pdf}
+        make run < $infile 2> $dotfile > /dev/null
+        dot -Tpdf $dotfile -o $pdffile 2> /dev/null
+    done
+done 
+
+# for infile in ls $IN/*.pas; do
+#     base=$(basename $infile)
+#     echo Running $base
+#     dotfile=$OUT/${base/.pas/.dot}
+#     make run < $infile 2> $dotfile > /dev/null
+#     dot -Tpdf $dotfile -o $OUT/$base.pdf 
+# done
